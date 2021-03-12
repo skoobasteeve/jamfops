90399b44508776ced7c72bf863d4de232aa71829
Ray Lyon <36998292+skoobasteeve@users.noreply.github.com>
2021-03-12 14:13:56 -0500

    fix URL

--- ext-attributes/onboarding-group-name.sh
+++ ext-attributes/onboarding-group-name.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Intended to be used with the onboarding script (https://github.com/skoobasteeve/jamfops/scripts/onboarding.sh)
+# Intended to be used with the onboarding script (https://github.com/skoobasteeve/jamfops/blob/main/scripts/jamf-onboarding.sh)
 
 for file in /tmp/.Onboarding/*; do
 	Team="${file##*/}"
